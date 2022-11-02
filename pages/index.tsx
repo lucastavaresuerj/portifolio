954f8aff01ec1b821b812e9f992379ccaaff1fcf
Lucas Tavares <lucastavaresuerj2016@gmail.com>
2022-11-02 12:48:17 -0300

    Add the next.js and some cloud formation templates

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Header } from "semantic-ui-react";
+
+export default function Home() {
+  return (
+    <React.Fragment>
+      <Header>Olá, eu sou o Lucas</Header>
+    </React.Fragment>
+  );
+}
